f3e6680eeca13251b78cc7745e07a6e7bfd6c6fa
agabek <agabek309@gmail.com>
2024-08-14 10:44:40 +0500

    update

--- internal/adapters/grpc/handler.go
+++ internal/adapters/grpc/handler.go
@@ -6,24 +6,21 @@ import (
 	"fmt"
 	pb "github.com/antibomberman/mego-protos/gen/go/storage"
 	"github.com/minio/minio-go/v7"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"math/rand"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
 func (s *serverAPI) PutObject(ctx context.Context, req *pb.PutObjectRequest) (*pb.PutObjectResponse, error) {
+	fileName := generateRandomFileName(s.storage, ctx, s.cfg.MinioBucket, req.GetFileName())
 
-	_, err := s.storage.StatObject(ctx, s.cfg.MinioBucket, req.GetFileName(), minio.StatObjectOptions{})
-	if err == nil {
-		return nil, status.Errorf(codes.AlreadyExists, "File %s already exists", req.GetFileName())
-	}
-
-	_, err = s.storage.PutObject(ctx, s.cfg.MinioBucket, req.GetFileName(), bytes.NewReader(req.GetData()), int64(len(req.GetData())), minio.PutObjectOptions{ContentType: req.ContentType})
+	_, err := s.storage.PutObject(ctx, s.cfg.MinioBucket, fileName, bytes.NewReader(req.GetData()), int64(len(req.GetData())), minio.PutObjectOptions{ContentType: req.ContentType})
 	if err != nil {
 		return nil, err
 	}
 
-	return &pb.PutObjectResponse{FileName: req.GetFileName()}, nil
+	return &pb.PutObjectResponse{FileName: fileName}, nil
 }
 
 func (s *serverAPI) GetObject(ctx context.Context, req *pb.GetObjectRequest) (*pb.GetObjectResponse, error) {
@@ -62,4 +59,27 @@ func (s *serverAPI) DeleteObject(ctx context.Context, req *pb.DeleteObjectReques
 	}
 
 	return &pb.DeleteObjectResponse{Message: "Successfully deleted " + req.GetFileName()}, nil
+}
+
+func generateRandomFileName(storage *minio.Client, ctx context.Context, bucketName, objectName string) string {
+
+	_, err := storage.StatObject(ctx, bucketName, objectName, minio.StatObjectOptions{})
+	if err == nil {
+		//exist
+		ext := filepath.Ext(objectName)
+		name := strings.TrimSuffix(objectName, ext)
+
+		for strings.Contains(name, ".") {
+			ext = filepath.Ext(name) + ext
+			name = strings.TrimSuffix(name, ext)
+		}
+
+		rand.Seed(time.Now().UnixNano())
+		randomString := fmt.Sprintf("%s_%d", name, rand.Intn(10000))
+
+		return generateRandomFileName(storage, ctx, bucketName, randomString+ext)
+	}
+
+	return objectName
+
 }
